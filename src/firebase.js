b51d9998cac66ab52a79c15e425d4b21b9b0ffc4
Patrickeon <01051188129e@gmail.com>
2021-12-07 19:37:27 +1000

    update

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC96zbHbNDShhRPSbMzl0DioxoXgcr1Er8",
+  authDomain: "nwitter-ac597.firebaseapp.com",
+  projectId: "nwitter-ac597",
+  storageBucket: "nwitter-ac597.appspot.com",
+  messagingSenderId: "43402102738",
+  appId: "1:43402102738:web:650c4fa17784f0c850aa0b",
+};
+
+export default initializeApp(firebaseConfig);
